cc804d662d043d6259aaf65335410045779650e8
Tom ONeill <46326884+tommyoneill@users.noreply.github.com>
2025-04-22 15:53:10 -0500

    Added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -78,6 +79,7 @@ export default function RootLayout({
         <TRPCReactProvider>
           {children}
           <Analytics />
+          <SpeedInsights />
         </TRPCReactProvider>
       </body>
     </html>
